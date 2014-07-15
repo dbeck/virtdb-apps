e8fc41d751462a2964b73505033cf6faac38f590
David Beck <dbeck@starschema.net>
2014-07-15 13:55:40 +0200

    more node.js integration

--- cmake/NodeExternal.cmake
+++ cmake/NodeExternal.cmake
@@ -1,17 +1,22 @@
 MESSAGE(STATUS "Integrating external Node.js sources")
 
 SET(NODE_MODULE "3rd-party/nodejs")
-SET(NODE_GIT "git@github.com:starschema/node.git")
+SET(NODE_GIT "https://github.com/starschema/node.git")
 
 ExternalProject_Add(
   nodejs
-  SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${NODE_MODULE}/
+  GIT_REPOSITORY
+    ${NODE_GIT}
+  GIT_SUBMODULES
+    ${NODE_MODULE}
+  SOURCE_DIR
+    ${CMAKE_CURRENT_SOURCE_DIR}/${NODE_MODULE}
   CONFIGURE_COMMAND 
     ${CMAKE_CURRENT_SOURCE_DIR}/${NODE_MODULE}/configure
     --prefix=${CMAKE_CURRENT_SOURCE_DIR}/lib/nodejs/
   PREFIX
     ${CMAKE_CURRENT_SOURCE_DIR}/lib/nodejs/
-  BUILD_COMMAND make
+  BUILD_COMMAND ${CMAKE_MAKE}
   BUILD_IN_SOURCE 1
 )
 
