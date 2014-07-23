f924760c4ac6f4f375a659fe9945a8474906de19
David Beck <dbeck@starschema.net>
2014-07-23 14:54:21 +0200

    restructuring KeyValue proto

--- src/testme.cc
+++ src/testme.cc
@@ -1,10 +1,13 @@
 #include "meta_data.pb.h"
+#include "common.pb.h"
 #include <memory>
 
 int main()
 {
   using virtdb::interface::pb::MetaDataRequest;
+  using virtdb::interface::pb::KeyValue;
   std::shared_ptr<MetaDataRequest> request(new MetaDataRequest);
+  std::shared_ptr<KeyValue>        key_value(new KeyValue);
   return 0;
 }
 
