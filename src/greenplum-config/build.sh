ef29bfb3d0a5a69338be3d1ade1a80ef6487c2f8
szilardhuber <szilard.huber@starschema.net>
2014-10-30 14:03:25 +0100

    Further improvements to gpconfig CI

--- src/greenplum-config/build.sh
+++ src/greenplum-config/build.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
+GPCONFIG_PATH="src/greenplum-config"
+NODE_CONNECTOR_PATH="src/common/node-connector"
+
+function release {
+  echo "release"
+  pushd $GPCONFIG_PATH
+  npm version patch
+  popd
+}
+
+function clear_connector {
+  echo "clearining node connector"
+  rm -rf $NODE_CONNECTOR_PATH/node_modules
+  rm -rf $NODE_CONNECTOR_PATH/lib
+}
+
+function clear_greenplum_config {
+  echo "Clearing greenplum config"
+  rm -rf $GPCONFIG_PATH/node_modules
+  rm -rf $GPCONFIG_PATH/out
+}
+
+[[ ${1,,} == "release" ]] && RELEASE=true || RELEASE=false
 
 gyp --depth=. apps.gyp
 make
 
 echo "Building node-connector"
-pushd src/common/node-connector
+[[ $RELEASE == true ]] && clear_connector
+pushd $NODE_CONNECTOR_PATH
 npm install
 node_modules/gulp/bin/gulp.js build
 popd
 
 echo "Building greenplum-config"
-pushd src/greenplum-config
+pushd $GPCONFIG_PATH
+[[ $RELEASE == true ]] && clear_greenplum_config 
 npm install
 npm install ../common/node-connector
 node_modules/gulp/bin/gulp.js build
 popd
+
+[[ $RELEASE == true ]] && release || echo "non-release"
